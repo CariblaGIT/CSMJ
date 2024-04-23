4b48605e5031ee7744123683354ba80c6226e146
MartaGBayona <marta.g.bayona@outlook.es>
2024-04-23 20:47:34 +0200

    feat:create migration room, model room and factory room success

--- database/migrations/2024_04_23_143608_create_rooms_table.php
+++ database/migrations/2024_04_23_143608_create_rooms_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+            $table->unsignedBigInteger('game_id');
+
+            $table->foreign('game_id')->references('id')->on('games');
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('rooms');
     }
 };
