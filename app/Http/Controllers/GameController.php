500451a8464f70860ac3a5ea4ad06314e6b74d09
Caribla <caribla4@gmail.com>
2024-04-23 19:46:29 +0200

    Feat: Games => Endpoint delete a game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -74,5 +74,21 @@ class GameController extends Controller
         }
     }
 
+    public function deleteGame ($id){
+        try {
+            $game_deleted = Game::destroy($id);
+
+            return response()->json([
+                "success" => true,
+                "message" => "Game deleted successfully"
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                "message" => "Game not updated",
+                "error" => $th->getMessage()
+            ], 500);
+        }
+    }
 }
 
--- routes/games.php
+++ routes/games.php
@@ -9,3 +9,5 @@ Route::get('/games', [GameController::class, 'getAllGames']);
 Route::post('/games', [GameController::class, 'postGame']);
 
 Route::put('/games/{id}', [GameController::class, 'putGame']);
+
+Route::delete('/games/{id}', [GameController::class, 'deleteGame']);
