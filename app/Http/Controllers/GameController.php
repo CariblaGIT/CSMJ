a72cf2572f3a909e6eaecf2eaf3166a8c44d9416
Caribla <caribla4@gmail.com>
2024-04-23 19:45:59 +0200

    Feat: Games => Endpoint update a game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -49,5 +49,30 @@ class GameController extends Controller
         }
     }
 
+    public function putGame (Request $req, $id){
+        try {
+            $game = Game::find($id);
+
+            $updateData = $req->only(['name', 'description', 'image']);
+            // $new_name = $req->input('name');
+            // $new_description = $req->input('description');
+            // $new_image = $req->input('image');
+
+            $game->update($updateData);
+
+            return response()->json([
+                "success" => true,
+                "message" => "Game updated successfully",
+                "data" => $game
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                "message" => "Game not updated",
+                "error" => $th->getMessage()
+            ], 500);
+        }
+    }
+
 }
 
--- routes/games.php
+++ routes/games.php
@@ -7,3 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/games', [GameController::class, 'getAllGames']);
 
 Route::post('/games', [GameController::class, 'postGame']);
+
+Route::put('/games/{id}', [GameController::class, 'putGame']);
