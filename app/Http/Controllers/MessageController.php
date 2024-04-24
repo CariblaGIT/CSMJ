fad668ea606255c5408dda086b83075956064f52
stg <a.sergiotorres@gmail.com>
2024-04-24 22:52:18 +0200

    Update: Message CRUD ->  getAllMyMessages and getAllRoomMessages instead of getAllMessages. Modified postMessage. Added updateMessageById

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,14 +7,37 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    public function getAllMessages()
+    public function getAllMyMessages()
     {
         try {
-            $messages = Message::all();
+            // Fetch messages posted by the current user
+            $userId = auth()->user()->id;
+            $messages = Message::where('user_id', $userId)->get();
 
             return response()->json([
                 "success" => true,
-                "message" => "Messages retrieved successfully",
+                "message" => "All your messages retrieved successfully",
+                "data" => $messages
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                "message" => "Messages not retrieved",
+                "error" => $th->getMessage()
+            ], 500);
+        }
+    }
+
+
+    public function getAllRoomMessages($room_id)
+    {
+        try {
+            // Fetch messages posted in this current room
+            $messages = Message::where('room_id', $room_id)->get();
+
+            return response()->json([
+                "success" => true,
+                "message" => "All messages from the room retrieved successfully",
                 "data" => $messages
             ], 200);
         } catch (\Throwable $th) {
@@ -30,6 +53,8 @@ class MessageController extends Controller
     public function postMessage(Request $request)
     {
         try {
+            $userId = auth()->user()->id;
+
             $request->validate([
                 'content' => 'required',
                 'user_id' => 'required',
@@ -38,8 +63,41 @@ class MessageController extends Controller
 
             $message = new Message;
             $message->content = $request->input('content');
-            $message->user_id = $request->input('user_id');
             $message->room_id = $request->input('room_id');
+            $message->user_id = $userId;
+
+            $message->save();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Message created successfuly',
+                'data' => $message
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Message couldn´t be created successfuly',
+                'data' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+
+    public function updateMessageById(Request $request, $id)
+    {
+        try {
+            $userId = auth()->user()->id;
+            $message = Message::find($id)->get();
+
+            $request->validate([
+                'content' => 'required',
+                'room_id' => 'required',
+                'user_id' => 'required'
+            ]);
+
+            $message->content = $request->input('content');
+            $message->room_id = $request->input('room_id');
+            $message->user_id = $userId;
 
             $message->save();
 
@@ -57,15 +115,10 @@ class MessageController extends Controller
         }
     }
 
-
-    // updateMessageById - TO BE DONE?
-
     public function deleteMessageById($id)
     {
         try {
             $messageDeleted = Message::destroy($id);
-            // $messageDeleted = Message::find($id);
-            // $messageDeleted->delete();
 
             return response()->json([
                 'success' => true,
--- routes/messages.php
+++ routes/messages.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\MessageController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 //MESSAGES
 
-Route::get('/messages', [MessageController::class, 'getAllMessages']);
+Route::get('/messages', [MessageController::class, 'getAllMyMessages']);
+Route::get('/messages/{room}', [MessageController::class, 'getAllRoomMessages']);
 Route::post('/messages', [MessageController::class, 'postMessage']);
-// Route::put('/messages/{id}', [MessageController::class, 'putMessage']);  // maybe not needed?
+Route::put('/messages/{id}', [MessageController::class, 'updateMessageById']);
 Route::delete('/messages/{id}', [MessageController::class, 'deleteMessageById']);
