d71a4b0063bf127aafff685e54d36fad19cf2fa6
Caribla <caribla4@gmail.com>
2024-04-24 21:14:04 +0200

    Fix: Merge => Bad conflict solution fixed with dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            GameSeeder::class
+            GameSeeder::class,
             RoomSeeder::class
         ]);
     }
