0bf2348c9a983cd76309d46a0dca6e5ae547d4d4
stg <a.sergiotorres@gmail.com>
2024-04-24 19:38:57 +0200

    Feat: MessageSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,46 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // DB::table('users')->insert([
-        //     [
-        //         'name' => Str::random(10),
-        //         'email' => Str::random(10) . 'gmail.com',
-        //         'password' => "123456",
-        //     ]
-        // ]);
-
-        // DB::table('games')->insert([
-        //     [
-        //         'description' => Str::random(100),
-        //         'url_image' => 'www.' . Str::random(5) . '.com',
-        //     ]
-        // ]);
-
-        // DB::table('rooms')->insert([
-        //     [
-        //         'name' => Str::random(10),
-        //         'description' => Str::random(100),
-        //         'game_id' => rand(1, 6),
-        //     ]
-        // ]);
-
-        // DB::table('messages')->insert([
-        //     [
-        //         'content' => fake()->sentence(),
-        //         'user_id' => "1",
-        //         'room_id' => "1",
-        //     ]
-        // ]);
-
-        // DB::table('users_rooms')->insert([
-        //     [
-        //         'user_id' => rand(1, 10),
-        //         'room_id' => rand(1, 10),
-        //     ]
-        // ]);
-
-        \App\Models\Message::factory(10)->create();
+        $this->call([
+            MessageSeeder::class
+        ]);
     }
 }
