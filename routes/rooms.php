16fa127bfbfc9fc9c93430bd038466f138eac3b3
Caribla <caribla4@gmail.com>
2024-04-26 11:59:56 +0200

    Feat: Rooms => Adding middlewares to rooms entity

--- routes/rooms.php
+++ routes/rooms.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/rooms', [RoomController::class, 'getAllRooms']);
-
-Route::post('/rooms', [RoomController::class, 'postRoom']);
-
-Route::put('/rooms/{id}', [RoomController::class, 'putRoom']);
-
-Route::delete('/rooms/{id}', [RoomController::class, 'deleteRoom']);
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function () {
+    Route::get('/rooms', [RoomController::class, 'getAllRooms']);
+    Route::post('/rooms', [RoomController::class, 'postRoom']);
+    Route::put('/rooms/{id}', [RoomController::class, 'putRoom']);
+    Route::delete('/rooms/{id}', [RoomController::class, 'deleteRoom']);
+});
