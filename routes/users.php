46183b29dec425189e32d7e639f975317357d375
Caribla <caribla4@gmail.com>
2024-04-27 15:52:32 +0200

    Feat: Users => Adding middlewares to users entity

--- routes/users.php
+++ routes/users.php
@@ -7,20 +7,14 @@ use Illuminate\Support\Facades\Route;
 
 //USERS
 
-Route::get('/users', [UserController::class, 'getAllUsers']);
-// Route::get('/users', function (){
-//     return 'GET ALL USERS';
-// });
-
+Route::get('/users', [UserController::class, 'getAllUsers'])->middleware('auth:sanctum')->middleware('super_admin');
 
 Route::get('/users/profile/', [UserController::class, 'getUserProfile'])->middleware('auth:sanctum');
 
-// Route::put('/users/{id}', [UserController::class, 'putUser']);
 Route::put('/users/{id}', function($id){
     return 'update USER' .$id;
 });
 
-// Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
 Route::delete('/users/{id}', function($id){
     return 'delete USER' .$id;
 });
